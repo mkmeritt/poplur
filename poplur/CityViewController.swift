95bf48480c6e49c621e33f2a5ca47eaacad6948b
Mark Meritt <mkmeritt93@gmail.com>
2016-08-15 13:02:07 -0400

    updated

---
+++ poplur/CityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CityViewController.swift
+//  poplur
+//
+//  Created by Mark Meritt on 2016-08-11.
+//  Copyright © 2016 Apptist. All rights reserved.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
